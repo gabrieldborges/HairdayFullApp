24561132103a0423955a2988ee5471b887546195
gabrieldborges <gabrieldantas1404@icloud.com>
2026-04-03 11:19:37 -0300

    feat : Implementing Text Component

---
+++ src/components/UiComponents/Text.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+const TextVariants = cva("text-gray-900", {
+    variants: {
+        variant: {
+            "title-lg-bold": "text-[2rem] leading-6 font-bold",
+            "title-md-bold": "text-[1rem] leading-6 font-bold",
+            "title-sm-bold": "text-[0.875rem] leading-5 font-bold",
+            "text-md-regular": "text-[1rem] leading-6 font-normal",
+            "text-sm-regular": "text-[0.875rem] leading-5 font-normal",
+        }
+    },
+    defaultVariants: {
+        variant: "text-md-regular"
+    }
+})
+
+interface TextProps extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof TextVariants> {
+    as?: keyof React.JSX.IntrinsicElements
+}
+
+export default function Text({ as = "span", className, children, variant, ...props }: TextProps) {
+    return (
+        React.createElement(as, { className: TextVariants({ variant, className }), ...props }, children)
+    )
+}
--- src/pages/pageComponents.tsx
+++ src/pages/pageComponents.tsx
@@ -1,12 +1,20 @@
 import Icon from "../components/UiComponents/Icon"
 import { iconsLib } from "../components/UiComponents/Icon"
+import Text from "../components/UiComponents/Text"
 
 export default function PageComponents() {
     return (
         <div className="flex flex-col gap-4">
             <div className="text-gray-500">
-                <h1>Page home</h1>
+                <h1>Page components</h1>
                 <Icon svg={iconsLib.calendar}></Icon>
+            </div>
+            <div className="flex flex-col gap-1">
+                <Text variant={"title-lg-bold"}>This is a Text Component</Text>
+                <Text variant={"title-md-bold"}>This is a Text Component</Text>
+                <Text variant={"title-sm-bold"}>This is a Text Component</Text>
+                <Text variant={"text-md-regular"}>This is a Text Component</Text>
+                <Text variant={"text-sm-regular"}>This is a Text Component</Text>
             </div>
         </div>
     )
